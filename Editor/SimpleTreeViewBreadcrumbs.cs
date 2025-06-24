e3f8f0753c0eff168e48f61f7433fc812770121d
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2025-06-24 03:01:16 +0200

    ⠀⠀⠀⠀⠀

--- Editor/SimpleTreeViewBreadcrumbs.cs
+++ Editor/SimpleTreeViewBreadcrumbs.cs
@@ -6,6 +6,7 @@ namespace UnityEssentials
 {
     public static class SimpleTreeViewBreadcrumbs
     {
+
         public static void Draw(SimpleTreeViewItem current, System.Action<SimpleTreeViewItem> onClick = null)
         {
             if (current == null)
@@ -18,6 +19,8 @@ namespace UnityEssentials
                 chain.Insert(0, node);
                 node = node.Parent;
             }
+
+            var displayNameCache = new List<string>();
 
             GUILayout.BeginHorizontal();
             for (int i = 0; i < chain.Count; i++)
@@ -33,9 +36,25 @@ namespace UnityEssentials
                         buttonRect.y += 20;
                         buttonRect.x = Event.current.mousePosition.x;
 
+
                         var menu = new GenericMenu();
                         foreach (var child in item.Parent.Children)
-                            menu.AddItem(new GUIContent(child.displayName), child == item, () => onClick?.Invoke(child));
+                        {
+                            string displayName = child.displayName;
+                            if (!displayNameCache.Contains(displayName))
+                                displayNameCache.Add(displayName);
+                            else
+                            {
+                                int increment = 1;
+                                while (displayNameCache.Contains($"{child.displayName} ({increment})"))
+                                    increment++;
+
+                                displayName = $"{child.displayName} ({increment})";
+                                displayNameCache.Add(displayName);
+                            }
+
+                            menu.AddItem(new GUIContent(displayName), child == item, () => onClick?.Invoke(child));
+                        }
 
                         menu.DropDown(buttonRect);
                     }
